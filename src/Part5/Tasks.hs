b4dd03fe79c76dcf31347c21151372c80aa56a95
kuzo-liza <kuzo-liza@yandex.ru>
2022-12-28 11:58:41 +0300

    almost everything in part 5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,28 +4,36 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl f z [] = z
+myFoldl f z (h:t) = myFoldl f (f z h) t
+
+-- пояснение:  myFoldl f a [x,y,z] = ((a `f` x) `f` y) `f` z)
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr f z [] = z
+myFoldr f z (h:t) = f h (myFoldr f z t)
+
+-- пояснение:  myFoldl f a [x,y,z] = x `f` (y `f` (z `f` a))
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f list = myFoldr (\y ys -> (f y):ys) [] list
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f list = myConcat (myMap f list)
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat list = myFoldr (++) [] list
+
+-- оператор конкатенации ++ принимает в качестве операндов 2 списка и объединяет в 1
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse list = myFoldl (\t h -> h:t) [] list
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p xs = myFoldr (\y x -> if p y then y:x else x) [] xs
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
 myPartition p = notImplementedYet
